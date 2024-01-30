32d6e782d79f8c6031c19b66aeed008e1951a75b
Ulysses Carlos <ulysses_carlos@protonmail.com>
2024-01-29 22:47:55 -0500

    02-Fundamentals-Part-2 Code as of 2024-01-29

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,35 @@
+/*
+ * ------------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 01/10/2024 at 09:28 PM
+ *
+ * script.js
+ *
+ * ------------------------------------------------------------------------------
+ */
+
+// Make sure to use strict mode.
+'use strict';
+
+function test() {
+    let hasDriversLicense = false;
+    const passTest = true;
+
+    if (passTest)
+	hasDriversLicense = true;
+
+    if (hasDriversLicense)
+	console.log("I am drive :D");
+
+    // const private = "Audio";
+}
+
+
+function main() {
+    test();
+    
+
+}
+
+
+// Now run the program:
+main();
