4c97a1a736d473decda9ebbffc67c1149f5e59a5
Ulysses Carlos <ulysses_carlos@protonmail.com>
2024-07-15 22:41:40 -0400

    2024-07-15 -- Lecture 75: Manipulating CSS Styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,18 +9,7 @@
 
 'use strict';
 
-function scratch() {
-    /*
-        document.querySelector('div.number').textContent = "420";
-        document.querySelector('input.guess').value = 69;
-        console.log(document.querySelector('p.message'));
-    */
-
-
-
-}
-
-
+let currentScoreValue = 20;
 function RandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -30,6 +19,35 @@ let secretNumber = RandomInt(1, 20);
 document.querySelector('div.number').textContent = secretNumber;
 
 
+function decreaseScore() {
+    let element = document.querySelector('span.score');
+    let newValue = currentScoreValue - 1;
+
+    // We prevent negative scores.
+    if (newValue > 0) {
+        element.textContent = `${newValue}`;
+        currentScoreValue = newValue;
+    }
+    else {
+        window.alert("You lost the game :(");
+        // Now reload the game.
+        location.reload();
+    }
+
+
+}
+
+function CompleteTheGameAndSetNewHighScore() {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('div.number').style.width = '30rem';
+
+
+    let currentScoreElement = document.querySelector('span.score');
+    let highScoreElement = document.querySelector('span.highscore');
+    highScoreElement.textContent = currentScoreElement.textContent;
+
+}
+
 const checkClickListener = () => {
     let value = Number(document.querySelector('input.guess').value);
     if (!value) {
@@ -38,23 +56,32 @@ const checkClickListener = () => {
     else {
         console.log(`Guess: ${value}\tType: ${typeof value}\n`);
 
-        if (value === secretNumber)
+        if (value === secretNumber) {
             document.querySelector(".message").textContent = "Yes, that is correct!";
-        else if (value > secretNumber)
+            CompleteTheGameAndSetNewHighScore();
+        }
+        else if (value > secretNumber) {
             document.querySelector(".message").textContent = "The number is too high!";
-        else
+            decreaseScore();
+        }
+        else {
             document.querySelector(".message").textContent = "The number is too low!";
+            decreaseScore();
+        }
     }
 };
 
-const repeatGameClickListener = () => {
-    secretNumber = RandomInt(1, 20);
-    document.querySelector('div.number').textContent = secretNumber;
-    document.querySelector('p.message').textContent = 'Start guessing...';
+
+function main() {
+    const repeatGameClickListener = () => {
+        secretNumber = RandomInt(1, 20);
+        document.querySelector('div.number').textContent = secretNumber;
+        document.querySelector('p.message').textContent = 'Start guessing...';
+    }
+
+    document.querySelector('.check').addEventListener('click', checkClickListener);
+    document.querySelector('button.btn.again').addEventListener('click', repeatGameClickListener);
 }
 
-
-document.querySelector('.check').addEventListener('click', checkClickListener);
-document.querySelector('button.btn.again').addEventListener('click', repeatGameClickListener);
-
-
+// Now run the program:
+main();
