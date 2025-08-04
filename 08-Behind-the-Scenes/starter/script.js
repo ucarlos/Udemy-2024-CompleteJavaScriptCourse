74d11d573181967b890d7ec23edca05b8facdfd8
Ulysses Carlos <ulysses_carlos@protonmail.com>
2025-08-04 14:36:05 -0400

    2025-08-04: Lecture 99: Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,11 +1,32 @@
 'use strict';
 
-const Common = {
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
 
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if ((1981 <= birthYear && birthYear <= 1996)) {
+            var millennial = true;
+            // strange scope
+            //const firstName = "Steven";
+
+            const str = `Oh, and you're a millennial, ${firstName}.`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+
+        }
+
+        //console.log(str);
+        console.log(millennial);
+    }
+    printAge();
+    return age;
 }
 
-
-
-// now run the stuff.
-
-
+const firstName = "Jonas";
+calcAge(1991);
